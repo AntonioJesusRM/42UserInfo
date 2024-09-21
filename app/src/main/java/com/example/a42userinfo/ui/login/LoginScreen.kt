8b3e32e581c8c6684e5e831d0a4734a659f376a9
AntonioJesusRM <antjrm95@gmail.com>
2024-09-21 11:18:06 +0200

    login layout

--- app/src/main/java/com/example/a42userinfo/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/a42userinfo/ui/login/LoginScreen.kt
@@ -1,14 +1,48 @@
 package com.example.a42userinfo.ui.login
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a42userinfo.R
+import com.example.a42userinfo.ui.components.CustomButton
 
 @Composable
 fun LoginScreen(
     onClickLogin: () -> Unit = {},
 ) {
-    Button(onClick = onClickLogin) {
-        Text(text = "Ir a home")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo42),
+            contentDescription = "Logo 42",
+            modifier = Modifier
+                .size(150.dp)
+                .padding(bottom = 32.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        CustomButton(text = stringResource(R.string.login_button), onClick = onClickLogin)
     }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
 }
