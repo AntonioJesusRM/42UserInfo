484c63981f2f5fb4f779b6d5939a9e29bd76d90e
AntonioJesusRM <antjrm95@gmail.com>
2024-09-21 11:09:02 +0200

    add color coalitions

--- app/src/main/java/com/example/a42userinfo/MainActivity.kt
+++ app/src/main/java/com/example/a42userinfo/MainActivity.kt
@@ -8,13 +8,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.a42userinfo.ui.theme._42UserInfoTheme
+import com.example.a42userinfo.ui.theme.UserInfoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            _42UserInfoTheme {
+            UserInfoTheme("") {
                 UserInfoApp()
             }
         }
