68d956c5c812ab5f8703b6d2c8dc5d5c927b9fa8
AntonioJesusRM <antjrm95@gmail.com>
2024-09-26 10:28:51 +0200

    go back button

--- app/src/main/java/com/example/a42userinfo/MainActivity.kt
+++ app/src/main/java/com/example/a42userinfo/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.a42userinfo
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,10 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.a42userinfo.ui.theme.UserInfoTheme
@@ -21,6 +16,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var authCode: String? = null
+        intent.data?.let { uri ->
+            if (uri.toString().startsWith("com.example.a42userinfo://oauth/callback")) {
+                authCode = uri.getQueryParameter("code")
+            }
+        }
+
         setContent {
             UserInfoTheme("") {
                 Surface(
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.secondary,
                     contentColor = MaterialTheme.colorScheme.primary
                 ) {
-                    UserInfoApp(intent)
+                    UserInfoApp(authCode)
                 }
             }
         }
@@ -36,20 +39,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun UserInfoApp(intent: Intent) {
-    var authCode by remember { mutableStateOf<String?>(null) }
+fun UserInfoApp(authCode: String?) {
     val navController = rememberNavController()
-    intent.data?.let { uri ->
-        if (uri.toString().startsWith("com.example.a42userinfo://oauth/callback")) {
-            val code = uri.getQueryParameter("code")
-            if (code != null) {
-                authCode = code
-            }
-        }
-    }
-    if (authCode == null) {
-        MainNavHost(navController = navController, code = null)
-    } else {
-        MainNavHost(navController = navController, code = authCode)
-    }
+
+    MainNavHost(navController = navController, code = authCode)
 }
